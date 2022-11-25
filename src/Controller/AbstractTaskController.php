eef251602b0373541425906d75048a222e566644
Clark Derhaag <clark@effectconnect.com>
2022-11-25 13:34:21 +0100

    Use Symfony MessageBusInterface instead of deprecated SW MonitoringBusDecorator.

--- src/Controller/AbstractTaskController.php
+++ src/Controller/AbstractTaskController.php
@@ -4,19 +4,28 @@ namespace EffectConnect\Marketplaces\Controller;
 
 use EffectConnect\Marketplaces\ScheduledTask\CatalogExportTask;
 use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\MessageQueue\MonitoringBusDecorator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AbstractTaskController extends AbstractController
 {
+    /**
+     * @var MessageBusInterface
+     */
     protected $messageBus;
 
-    public function __construct(MonitoringBusDecorator $messageBus)
+    public function __construct(MessageBusInterface $messageBus)
     {
         $this->messageBus = $messageBus;
+    }
+
+    private function dispatch($message, array $stamps = []): Envelope
+    {
+        return $this->messageBus->dispatch(Envelope::wrap($message, $stamps), $stamps);
     }
 
     /**
@@ -29,12 +38,10 @@ class AbstractTaskController extends AbstractController
 
         switch ($type) {
             case 'catalog':
-                $this->messageBus->dispatch((new CatalogExportTask())->setSalesChannelId($salesChannelId));
-                break;
-            case 'offer':
-            case 'order':
+                $this->dispatch((new CatalogExportTask())->setSalesChannelId($salesChannelId));
+                return new JsonResponse(['status' => 'OK']);
+            default:
                 return new JsonResponse(['status' => 'NOK']);
         }
-        return new JsonResponse(['status' => 'OK']);
     }
 }
